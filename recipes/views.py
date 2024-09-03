49811b29fb8875c326ca5804a3842e7e17274c8b
Nathan Henrique <nathanhcr99@gmail.com>
2024-09-02 23:34:38 -0300

    Templates

--- recipes/views.py
+++ recipes/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request,'recipes/home.html')
+    return render(request,'recipes/pages/home.html')
 
 def contato(request):
     return HttpResponse('Contato')
